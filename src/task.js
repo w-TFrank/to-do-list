18c09f445bd79876bba0fa92698c3fbf39d6ae13
TFrank <madmonsterHI@gmail.com>
2023-08-24 15:20:22 -0400

    added assets. testing sample to-do items

--- src/task.js
+++ src/task.js
@@ -1,3 +1,6 @@
+import checkboxBlank from "./checkbox-blank.svg";
+import checkboxChecked from "./checkbox-checked.svg";
+
 export default class task {
     constructor (project, task, priority) {
         this.project = project;
@@ -5,13 +8,34 @@ export default class task {
         this.priority = priority;
     }
 
-    printOneLine() {
+
+    //trying to make this to make a sample task list item.
+    //right now i think it doesnt have the correct id's for the task divs
+    taskDomMaker() {
         const main = document.getElementById("tasks");
-        
-        const oneLine = document.createElement("div");
-    
-        const makeOneLine = this.project + this.task + this.priority;
-        oneLine.textContent = makeOneLine;
-        main.appendChild(oneLine);
+        const checkboxBlankAsset = new Image();
+        checkboxBlankAsset.src = checkboxBlank;
+        const checkboxCheckedAsset = new Image();
+        checkboxCheckedAsset.src = checkboxChecked;
+
+        let taskListItem = document.createElement("div");
+        let priorityAsset = document.createElement("div");
+        let checkboxDiv = document.createElement("div");
+        let checkboxAsset = document.createElement("img");
+
+        taskListItem.id = "task";
+        priorityAsset.id = "priority-box";
+        checkboxDiv.id = "checkbox";
+        checkboxAsset.src = checkboxBlankAsset;
+        checkboxAsset.alt = "blank checkbox";
+
+        //just to test
+        main.append(priorityAsset, checkboxDiv);
+        checkboxDiv.appendChild(checkboxAsset);
+        priorityAsset.textContent = "!";
+        taskListItem.textContent = this.task;
+    }
+
+    printTask() {
     }
 }
